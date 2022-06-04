e980f67d66addc10b29f1e90e411d96d6264c0c2
Jim Mason <jimmason564@yahoo.com>
2022-06-03 19:25:37 -0700

    Initial inquirer statement

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquire = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt()[
+     {
+      type:'input',
+      name: 'name',
+      message: 'What is your project`s name?'
+     }   
+    ]
