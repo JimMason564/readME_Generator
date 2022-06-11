f966b5edcafc9b9eaeb6f1237df7e66ca970847c
Jim Mason <jimmason564@yahoo.com>
2022-06-10 20:58:09 -0700

    Adding example radme

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -10,10 +10,18 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== 'None') {
-    return `*[License](#license)`
+  if (license === "MIT"){
+  return '[License: MIT](https://opensource.org/licenses/MIT)'
   }
-  return ' '
+  if (license === 'Apache License 2.0') {
+    return '[License: Apache](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license === 'GNU General Public License v3.0') {
+    return '[License: GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html)'
+  }
+  if (license === 'BSD 3-Clause NEW or REVISED License') {
+    return '[License: BSD 3-Clause NEW or REVISED License](https://opensource.org/licenses/BSD-3-Clause)'
+  }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -26,9 +34,31 @@ function renderLicenseSection(license) {
 }
 
 // TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.title}
-
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+${renderLicenseLink(data.license)}
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributing}
+## Test
+${data.test}
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+## Questions
+${data.git}
+${data.email}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,48 +1,66 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
 
-inquirer
-    .prompt()[
-     {
-      type:'input',
-      name: 'name',
-      message: 'What is your project`s name?'
-     },   
-     {
-        type:'input',
-        name: 'description',
-        message: 'Please describe your project'
-       },   
-        {
-        type:'input',
-        name: 'installation',
-        message: 'Please describe any libraries that need to be added in order for your app to work properly'
-       },     
-       {
-        type:'input',
-        name: 'Usage',
-        message: 'How does your app work?'
-       },     
-       {
-        type:'input',
-        name: 'license',
-        message: 'What licenses are needed?'
-       },    
-        {
-        type:'input',
-        name: 'contributing',
-        message: 'Were there any contributors to your project?'
-       },     
-       {
-        type:'input',
-        name: 'questions',
-        message: 'Please enter a link to your GitHub account'
-       },   
-       {
-         type:'input',
-         name: 'questions',
-         message: 'Please enter your e-mail address'
-        }   
-    ]
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project`s title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Please describe any libraries that need to be added in order for your app to work properly",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How does your app work?",
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Were there any contributors to your project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What tests were run to validate this project?",
+  },
+  {
+    type: "input",
+    name: "git",
+    message: "Please enter a link to your GitHub account",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your e-mail address",
+  },
+];
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(`${process.cwd()}/${fileName}`, data);
+}
 
-    
+function init() {
+  console.log("Hello");
+  inquirer.prompt(questions).then((res) => {
+    console.log("Creating");
+    writeToFile("test.md", generateMarkdown(res));
+  });
+}
+
+init();
